f736d2ff1665b20958abd73bc19b7f58da0cf0c8
j1n3 <j1n3@j1n3deMacBook-Air.local>
2025-09-26 15:58:07 +0800

    更新项目：本地优化和调整

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@
 import os  # 导入os模块，用于读取环境变量
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'cab5cfcd9cb8a1177a588a5c8879aafbea317fe8a4f81c8bb13006cabd4a475e'  # Flask安全密钥，用于会话等
+    SECRET_KEY = os.environ.get('SECRET_KEY')  # Flask安全密钥，必须通过环境变量设置
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/phishing.db'  # 数据库连接字符串，使用SQLite
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # 关闭SQLAlchemy事件系统，节省资源
